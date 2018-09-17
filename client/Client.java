b4f364c3630679c1d8b19ed7716d933ea380cace
elindfo <elindfo@kth.se>
2018-09-17 21:43:17 +0200

    Add server dc exception handling in client

--- client/Client.java
+++ client/Client.java
@@ -3,6 +3,7 @@ package lab2a.client;
 import lab2a.server.ChatServer;
 
 import java.net.MalformedURLException;
+import java.rmi.ConnectException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -78,6 +79,9 @@ public class Client extends UnicastRemoteObject implements ChatClient{
             }while(!message.equals("/quit"));
             System.out.println("Loop ended");
             //server.deRegisterForNotification(this);
+        } catch (ConnectException e) {
+            System.err.println("Server not responding");
+            System.exit(0);
         } catch (RemoteException e) {
             e.printStackTrace();
         } finally{
@@ -98,6 +102,7 @@ public class Client extends UnicastRemoteObject implements ChatClient{
             ChatServer server = (ChatServer) Naming.lookup("rmi://" + args[0] + "/chatServer");
             Client client = new Client(server);
             client.start();
+            System.out.println("Exiting...");
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (NotBoundException e) {
